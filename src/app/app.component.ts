3d1a814a565abe63893d3f1d36e018acbcce3f53
Venu Gopal Boddu <vb19419@corp.ojas-it.com>
2019-12-10 16:12:16 +0530

    Task11

---
+++ src/app/app.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core'; 
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  inputs:['activeColor','baseColor','overlayColor']
+})
+export class AppComponent  {
+
+  activeColor: string = 'green';
+    baseColor: string = '#ccc';
+    overlayColor: string = 'rgba(255,255,255,0.5)';
+    
+    dragging: boolean = false;
+    loaded: boolean = false;
+    imageLoaded: boolean = false;
+    imageSrc: string = '';
+    
+    handleDragEnter() {
+        this.dragging = true;
+    }
+    
+    handleDragLeave() {
+        this.dragging = false;
+    }
+    
+    handleDrop(e) {
+        e.preventDefault();
+        this.dragging = false;
+        this.handleInputChange(e);
+    }
+    
+    handleImageLoad() {
+        this.imageLoaded = true;
+    }
+
+    handleInputChange(e) {
+        var file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
+
+        var pattern = /image-*/;
+        var reader = new FileReader();
+
+        if (!file.type.match(pattern)) {
+            alert('invalid format');
+            return;
+        }
+
+        this.loaded = false;
+
+        reader.onload = this._handleReaderLoaded.bind(this);
+        reader.readAsDataURL(file);
+    }
+    
+    _handleReaderLoaded(e) {
+        var reader = e.target;
+        this.imageSrc = reader.result;
+        this.loaded = true;
+    }
+
+}
+
